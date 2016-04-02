9d1f6bb87a69ae7ade5301d8cc330c4eef107b84
oasi-adamay <oasi.adamay@gmail.com>
2016-04-02 19:47:46 +0900

    initial commit
    + gitignore

---
+++ hello_world/hello_world.cpp
@@ -0,0 +1,14 @@
+// hello_world.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+
+#include <stdio.h>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("hello world\n");
+	return 0;
+}
+
