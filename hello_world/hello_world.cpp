a40c733a88da72fce3fad476a940f0f348f1fe6a
Isao Yamada <yamada@deep-i.co.jp>
2017-04-28 19:40:38 +0900

    hoge

--- hello_world/hello_world.cpp
+++ hello_world/hello_world.cpp
@@ -1,12 +1,11 @@
 ﻿// hello_world.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
 //
 
-#include "stdafx.h"
 
 #include <stdio.h>
 
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char* argv[])
 {
 	printf("hello world\n");
 	printf("develop\n");
